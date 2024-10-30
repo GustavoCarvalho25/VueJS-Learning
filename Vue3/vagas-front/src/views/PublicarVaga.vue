600ea761ac7b9332127373666cdbd4afab9f138a
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-10-30 17:31:26 -0300

    @GustavoCarvalho25 - Criando tela componente PublicarVaga

--- Vue3/vagas-front/src/views/PublicarVaga.vue
+++ Vue3/vagas-front/src/views/PublicarVaga.vue
@@ -1,13 +1,87 @@
 <template>
-  <div>
-    <p>Publicar Vaga</p>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <h4>Apresente a sua vaga para milhares de profissionais e de graça</h4>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Titulo da Vaga</label>
+        <input type="text" class="form-control" v-model="titulo">
+        <div class="form-text">Por exemplo: Programador Javascript e VueJS.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Descrição</label>
+        <textarea type="text" class="form-control" v-model="descricao"> </textarea>
+        <div class="form-text">Informe os detalhes da vaga</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Salário</label>
+        <input type="number" class="form-control" v-model="salario">
+        <div class="form-text">Informe o salário.</div>
+      </div>
+
+      <div class="col">
+        <label class="form-label">Modalidade</label>
+        <select class="form-select" v-model="modalidade">
+          <option value="" disabled>Selecione</option>
+          <option value="1">Home office</option>
+          <option value="2">Presencial</option>
+        </select>
+        <div class="form-text">Informe onde as atividades serão realizadas.</div>
+      </div>
+
+      <div class="col">
+        <label class="form-label">Tipo</label>
+        <select class="form-select" v-model="tipo">
+          <option value="" disabled>Selecione</option>
+          <option value="1">CLT</option>
+          <option value="2">PJ</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <button type="submit" class="btn btn-primary" @click="salvarVaga()">Cadastrar</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'PublicarVaga',
+  data: () => ({
+    titulo: '',
+    descricao: '',
+    salario: '',
+    modalidade: '',
+    tipo: '',
+  }),
+  methods: {
+    salvarVaga() {
+      let vaga = {
+        titulo: this.titulo,
+        descricao: this.descricao,
+        salario: this.salario,
+        modalidade: this.modalidade,
+        tipo: this.tipo,
+      }
+    }
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+</style>/
