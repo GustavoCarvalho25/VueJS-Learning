4596b76bdc8b349e66dcf7f26798c603637b2cc0
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-10-31 18:01:34 -0300

    @GustavoCarvalho25 - Refatorando App

--- Vue3/vagas-front/src/App.vue
+++ Vue3/vagas-front/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <h1>Componente APP</h1>
-  <topo-padrao></topo-padrao>
-  <conteudo></conteudo>
+  <vagas-favoritas></vagas-favoritas>
+  <topo-padrao @navegar="componente = $event"></topo-padrao>
+  <conteudo :conteudo="componente"></conteudo>
 </template>
 
 <script>
+import VagasFavoritas from '@/comuns/VagasFavoritas.vue';
 import Conteudo from '@/components/layouts/Conteudo.vue'
 import TopoPadrao from '@/components/layouts/TopoPadrao.vue'
 
@@ -12,7 +13,18 @@ export default {
   name: 'App',
   components: {
     Conteudo,
-    TopoPadrao
+    TopoPadrao,
+    VagasFavoritas
+  },
+  data: () => ({
+    componente: 'Home'
+  }),
+  methods: {
+    acao(p1, p2) {
+      console.log('Função de callback definida no componente pai e chamada no componente filho')
+      console.log(p1)
+      console.log(p2)
+    }
   }
 }
 </script>
