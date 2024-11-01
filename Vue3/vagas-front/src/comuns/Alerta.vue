a7e635e38c18a12bf8e022b0e9da8fd53cceaf83
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-11-01 12:32:55 -0300

    @GustavoCarvalho25 - Criando componente alerta

---
+++ Vue3/vagas-front/src/comuns/Alerta.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert alert-success" role="alert">
+    Mensagem
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alerta'
+}
+</script>
+
+
+<style scoped>
+
+</style>
